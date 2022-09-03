1e37330bada45d8e2c89f8faf31af4d014b8e1e5
silvia <villalobos.veas@gmail.com>
2022-09-03 12:13:18 -0400

    practica

---
+++ 03_strings.py
@@ -0,0 +1,31 @@
+# Los stings son cadenas de letras
+# Se pueden hacer con "" o ''
+
+str_one = "Micro"
+str_two = 'System'
+
+# Podemos uno o mas strings se denomina concatenar.En python se puede concatenar utilizando el operador '+'
+print(str_one + " " + str_two)
+print(str_one + str_two)
+
+# Ojo con la ',' la función print NO
+# Contanena
+print(str_one, str_two)
+
+# Podemos tambien  multiplicar un string con un  int
+
+print('Hello'*3)
+
+str_three = 'Bye'
+
+result = str_three * 5
+
+print(result)
+
+# Existe la función len() que nos entrega el largo de un string
+
+print(len(result))
+
+
+
+
